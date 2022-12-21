5a51f9f7816730e9c66c9191b6897ac761130428
Joshua Ellis <joshua@wideeyecreative.com>
2022-12-21 16:42:36 -0500

    add mockend and renaming for use

--- fuse-plugin.php
+++ fuse-plugin.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Plugin Name:     Plugin Name
+ * Plugin Name:     Fuse
  * Plugin URI:      PLUGIN SITE HERE
  * Description:     PLUGIN DESCRIPTION HERE
  * Author:          YOUR NAME HERE
  * Author URI:      YOUR SITE HERE
- * Text Domain:     plugin-name
+ * Text Domain:     fuse
  * Domain Path:     /languages
  * Version:         0.1.0
  *
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'plugin_DIR', plugin_dir_path( __FILE__ ) );
-define( 'plugin_URL', plugin_dir_url( __FILE__ ) );
+define( 'FUSE_DIR', plugin_dir_path( __FILE__ ) );
+define( 'FUSE_URL', plugin_dir_url( __FILE__ ) );
 
 // Your code starts here.
